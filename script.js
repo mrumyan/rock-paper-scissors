924580320fceafd9bd537427af5a95ed7670e264
mrumyan <manechka96@list.ru>
2022-11-11 13:25:34 +0300

    feat: add game board and result screen

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+/***GAME LOGIC***/
 const options = ['rock', 'paper', 'scissors'];
 
-function getPlayerChoice() {
-    return prompt('Rock, paper or scissors?', options[0]);
-}
+// function getPlayerChoice() {
+//     return prompt('Rock, paper or scissors?', options[0]);
+// }
 
 function getComputerChoice() {
     return options[Math.floor(Math.random() * options.length)];
@@ -22,7 +23,7 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function getWinner(computerScore, playerScore) {
+function getWinner(playerScore, computerScore) {
     return computerScore > playerScore ? "You lose the game :(" : computerScore < playerScore ? "You win the game :)" : "It's a draw!";
 }
 
@@ -52,3 +53,84 @@ function game() {
 }
 
 //console.log(game());
+
+/***UI***/
+const startBtn = document.querySelector('#start');
+const screens = document.querySelectorAll('.screen');
+const playerChoices = document.querySelector('.game__options');
+const computerChoiseBtn = document.querySelector('.game__computer').querySelector('.game__option');
+
+const playerScoreBtn = document.querySelector('.player-score');
+// console.log(playerScoreBtn.textContent);
+const computerScoreBtn = document.querySelector('.computer-score');
+// console.log(computerScoreBtn.textContent);
+
+const gameStatus = document.querySelector('.game__status');
+
+const winner = document.querySelector('.result__winner');
+const startAgainBtn = document.querySelector('#start-again');
+
+let playerChoice = '';
+let computerChoice = '';
+
+let playerScore = 0;
+let computerScore = 0;
+
+function capitalizeFirstLetter(str) {
+    return str[0].toUpperCase().concat(str.slice(1));
+}
+
+function game(playerChoice, computerChoice) {
+    let roundWinner = playRound(playerChoice, computerChoice);
+    if (roundWinner === 'computer') {
+        computerScoreBtn.textContent = ++computerScore;
+        gameStatus.textContent = `You lose! ${capitalizeFirstLetter(computerChoice)} beats ${capitalizeFirstLetter(playerChoice)}`;
+    } else if (roundWinner === 'player') {
+        playerScoreBtn.textContent = ++playerScore;
+        gameStatus.textContent = `You win! ${capitalizeFirstLetter(playerChoice)} beats ${capitalizeFirstLetter(computerChoice)}`;
+    } else {
+        gameStatus.textContent = `It's a draw!`;
+    }
+}
+
+function cleanGameData() {
+    for (let screen of screens) {
+        screen.classList.remove('up');
+    }
+    playerChoice = '';
+    computerChoice = '';
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreBtn.textContent = playerScore;
+    computerScoreBtn.textContent = computerScore;
+    gameStatus.textContent = 'Pick an option';
+    computerChoiseBtn.classList = 'game__option';
+}
+
+startBtn.addEventListener('click', event => {
+    event.preventDefault();
+    screens[0].classList.add('up');
+});
+
+playerChoices.addEventListener('click', event => {
+    if (event.target.classList.contains('game__option')) {
+        gameStatus.textContent = '';
+        playerChoice = event.target.getAttribute('data-value');
+
+        computerChoice = getComputerChoice();
+        computerChoiseBtn.classList = `game__option ${computerChoice}`;
+
+        game(playerChoice, computerChoice);
+
+        if (playerScore >= 5 || computerScore >= 5) {
+            screens[1].classList.add('up');
+            winner.textContent = getWinner(playerScore, computerScore);
+        }
+    }
+});
+
+startAgainBtn.addEventListener('click', event => {
+    event.preventDefault();
+    cleanGameData();
+    screens[0].classList.add('up');
+});
